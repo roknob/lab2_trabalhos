e93597abf6df13da96c635207faa76fd019e8378
roberta <robertamariaknob@gmail.com>
2026-08-10 14:38:48 -0300

    estrtura inicial

--- roberta_t1.c
+++ roberta_t1.c
@@ -2,6 +2,35 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+typedef struct {
+  bool terminou_jogo;
+  bool terminou_onda;
+  bool terminou_partida;
+  int pontos;
+  int inimigos_inativos;
+  int tiros;
+  // ...
+} estado_t;
+
+void joga_onda(estado_t *est)
+{
+  //repete enquanto nao terminar a onda
+  {
+    processa_teclado(est);
+    processa_tempo(est);
+    apresenta(est);
+  }
+}
+
+void joga_partida(estado_t *est)
+{
+  while (!est->terminou_partida) {
+    joga_onda(est);
+  }
+}
 
 // configura o terminal para o modo "cru", para permitir a leitura
 //   de cada caractere digitado sem esperar pelo "enter".
@@ -49,5 +78,14 @@ int main()
         n++;
         printf("leituras: %d  ult ch: %d   \r", n, c);
     } while (u != 'q');
+
+    estado_t estado;
+    inicializa_tela();
+    inicializa_estado(&estado);
+    while (!estado.terminou_jogo) {
+        joga_partida(&estado);
+    }
+    desinicializa_tela();
+
     normaliza_terminal();
 }
