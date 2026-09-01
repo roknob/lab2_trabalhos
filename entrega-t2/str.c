e3ec363340121e710f692f780b7b6157fe64194a
roberta <roberta.knob@acad.ufsm.br>
2026-09-01 01:53:04 -0300

    finalizacao da s_substitui

--- entrega-t2/str.c
+++ entrega-t2/str.c
@@ -291,14 +291,72 @@ int s_busca_s(Str_c s, int pos, Str_c buscada)
   return -1;
 }
 
+static void verifica_posicao_substitui (Str s, int *pos, int *tam, Str_c sb)
+{
+  *pos = pos_busca(s, *pos);
+  if (*pos < 0){
+    *pos = 0;
+  } else if(*pos > s_tam(s)){
+    *pos = s_tam(s);
+  }
+  if (*tam < 0) {
+    *tam = s_tam(s) - *pos;
+  }
+  if (*pos + *tam > s_tam(s)){
+    *tam = s_tam(s) - *pos;
+  }
+}
 
-// operações de alteração {{{1
+static int qtd_bytes_substitui(Str s, int pos, int tam, Str_c sb)
+{
+  int qtd_bytes_sb;
+  if (sb==NULL){
+    qtd_bytes_sb = 0;
+  } else {
+    qtd_bytes_sb = sb->qtd_bytes_usados;
+  }
+  return qtd_bytes_sb;
+}
+
+static void atualiza_alocacao(Str s, int qtd_bytes_atualizada, int alocacao_atualizada)
+{
+  if(qtd_bytes_atualizada == 0){
+    free(s->bytes);
+    s_vazia(s);
+  } else if(qtd_bytes_atualizada > s->capacidade_bytes){
+    byte *tam_bytes_atualizada = realloc(s->bytes, alocacao_atualizada);
+    assert(tam_bytes_atualizada != NULL);
+    s->bytes = tam_bytes_atualizada;
+    s->capacidade_bytes = alocacao_atualizada;
+  }
+}
 
 void s_substitui(Str s, int pos, int tam, Str_c sb)
 {
   s_ok(s);
-  s_ok(sb);
-  //...
+  if (sb!=NULL){
+    s_ok(sb);
+  }
+  int qtd_bytes_pre_substitui = s->qtd_bytes_usados;
+  verifica_posicao_substitui(s, &pos, &tam, sb);
+  byte *p_pos_inicio = u8_avanca_unichar(s->bytes, pos);
+  int deslocamento_pos_i = p_pos_inicio - s->bytes;
+  byte *p_pos_fim = u8_avanca_unichar(s->bytes, pos + tam);
+  int deslocamento_pos_f = p_pos_fim - s->bytes;
+  int qtd_bytes_remover = deslocamento_pos_f - deslocamento_pos_i;
+  int qtd_bytes_sb = qtd_bytes_substitui(s, pos, tam, sb);
+  int qtd_bytes_atualizada = qtd_bytes_pre_substitui - qtd_bytes_remover + qtd_bytes_sb;
+  int alocacao_atualizada = calcula_alocacao(qtd_bytes_atualizada);
+  atualiza_alocacao(s, qtd_bytes_atualizada, alocacao_atualizada);
+  if(qtd_bytes_atualizada != 0){
+    int qtd_bytes_apos = qtd_bytes_pre_substitui - deslocamento_pos_f;
+    int destino_apos = deslocamento_pos_i + qtd_bytes_sb;
+    memmove(s->bytes + destino_apos, s->bytes + deslocamento_pos_f, qtd_bytes_apos);
+    if(sb != NULL){
+      memcpy(s->bytes + deslocamento_pos_i, sb->bytes, qtd_bytes_sb);
+    }
+    s->qtd_bytes_usados=qtd_bytes_atualizada;
+  }
 }
 
 void s_substring(Str s, Str_c sb, int pos, int tam)
